cff92995c20bfbe8ee1a003214775e12107d19a1
Paul Zaremba <pzaremba@vsb.bc.ca>
2019-09-17 10:38:55 -0700

    push

--- lessononeagain/src/Main.java
+++ lessononeagain/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(AssignmentOne.linearProblem(0,4,5));
         System.out.println("new stuff");
+        System.out.println("Some new code");
     }
 }
